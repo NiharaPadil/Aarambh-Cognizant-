7ddbd6b5b0ffd8cf294a13c77b6b0e6503f57403
Nihara Padil <niharapadilhr@gmail.com>
2024-10-09 09:49:18 +0530

    SplashScreen

--- aarambh-app/app/index.tsx
+++ aarambh-app/app/index.tsx
@@ -1,13 +1,38 @@
-import { Image, StyleSheet, Platform, Text } from 'react-native';
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import logo from '../assets/images/logo.png'; // Ensure the image file exists at this path
 
-export default function HomeScreen() {
+const SplashScreen = () => {
   return (
-    <Text style={styles.hi}>Hi Lima Adithi , from this page we have to start </Text>
+    <View style={styles.container}>
+      <Image 
+        source={logo} // Make sure to provide the correct path to your image
+        style={styles.image}
+      />
+      <Text style={styles.text}>Aarambh</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
- hi :{
-    color: 'red' 
- }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E0F0FF', // Light blue background similar to your image
+  },
+  image: {
+    width: 350,  // Adjust the width to fit your needs
+    height: 350, // Adjust the height as per the design
+    resizeMode: 'contain',
+  },
+  text: {
+    marginTop: 20,
+    fontSize: 48,
+   
+    fontWeight: 'bold',
+    color: '#000', // Black color for the text
+  }
 });
+
+export default SplashScreen; 
